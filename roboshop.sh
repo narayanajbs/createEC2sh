aedad51f2e30d4dc8031826a51a40ef35d100769
narayanajbs <narayana.jbs.com>
2024-10-20 21:15:44 +0530

    updated file

--- roboshop.sh
+++ roboshop.sh
@@ -8,6 +8,7 @@ ZONE_ID="Z02733572PIKGQZR8W2GI"
 DOMAIN_NAME="joindevopstest.online"
 for i in "${INSTANCE_NAME[@]}"; 
 do
+  echo "out put of the i >>>>>>>>>>>  $i"
     (aws ec2 run-instances --image-id $IMAGE_ID --instance-type $INSTANCE_TYPE --key-name "Ansible" --security-group-ids $SECURITY_GROUP_IDS --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
 
     echo "$i: $IP_ADDRESS"
